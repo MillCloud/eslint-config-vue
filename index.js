3d04d8cdadf0eac82f89853b0c72045ca49741d0
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-06-28 12:04:23 +0800

    use prettier and babel-eslint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
-  extends: ["eslint:recommended", "airbnb-base", "plugin:vue/recommended"],
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:vue/recommended'],
   env: {
     browser: true,
     es2020: true,
@@ -13,49 +13,49 @@ module.exports = {
     node: true,
   },
   globals: {
-    plus: "readonly",
-    uni: "readonly",
-    weex: "readonly",
+    plus: 'readonly',
+    uni: 'readonly',
+    weex: 'readonly',
   },
   rules: {
-    "no-console":
-      process.env.NODE_ENV === "production" ||
-      process.env.NODE_ENV === "pre-production" ||
-      process.env.NODE_ENV === "staging"
-        ? "warn"
-        : "off",
-    "no-debugger":
-      process.env.NODE_ENV === "production" ||
-      process.env.NODE_ENV === "pre-production" ||
-      process.env.NODE_ENV === "staging"
-        ? "warn"
-        : "off",
-    "no-param-reassign": [
-      "error",
+    'no-console':
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'pre-production' ||
+      process.env.NODE_ENV === 'staging'
+        ? 'warn'
+        : 'off',
+    'no-debugger':
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'pre-production' ||
+      process.env.NODE_ENV === 'staging'
+        ? 'warn'
+        : 'off',
+    'no-param-reassign': [
+      'error',
       {
         props: true,
         ignorePropertyModificationsFor: [
-          "state", // for vuex state
-          "acc", // for reduce accumulators
-          "e", // for e.returnvalue
+          'state', // for vuex state
+          'acc', // for reduce accumulators
+          'e', // for e.returnvalue
         ],
       },
     ],
-    "no-unused-vars":
-      process.env.NODE_ENV === "production" ||
-      process.env.NODE_ENV === "pre-production" ||
-      process.env.NODE_ENV === "staging"
-        ? "warn"
-        : "off",
-    "import/extensions": [
-      "error",
-      "always",
+    'no-unused-vars':
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'pre-production' ||
+      process.env.NODE_ENV === 'staging'
+        ? 'warn'
+        : 'off',
+    'import/extensions': [
+      'error',
+      'always',
       {
-        js: "never",
-        mjs: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
   },
