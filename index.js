e77e60c0aa32347b7480e2ef08fe49faa7538761
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-07-13 08:55:34 +0800

    release: 1.6.5

--- index.js
+++ index.js
@@ -47,13 +47,6 @@ module.exports = {
       process.env.NODE_ENV === 'staging'
         ? 'warn'
         : 'off',
-    'import/resolver': {
-      // https://github.com/benmosher/eslint-plugin-import/issues/1396
-      [require.resolve('eslint-import-resolver-node')]: {},
-      [require.resolve('eslint-import-resolver-webpack')]: {
-        config: require.resolve('@vue/cli-service/webpack.config.js'),
-      },
-    },
     'import/extensions': [
       'error',
       'always',
@@ -65,5 +58,15 @@ module.exports = {
         tsx: 'never',
       },
     ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.mjs', '.ts', '.tsx'],
+    'import/resolver': {
+      // https://github.com/benmosher/eslint-plugin-import/issues/1396
+      [require.resolve('eslint-import-resolver-node')]: {},
+      [require.resolve('eslint-import-resolver-webpack')]: {
+        config: require.resolve('@vue/cli-service/webpack.config.js'),
+      },
+    },
   },
 };
