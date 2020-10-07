8019b936d54f3192b04fd0d54c4fc9379b0d89e8
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-10-07 22:37:24 +0800

    add ts config

---
+++ typescript.js
@@ -0,0 +1,86 @@
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:vue/recommended',
+  ],
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    mocha: true,
+    node: true,
+  },
+  globals: {
+    plus: 'readonly',
+    uni: 'readonly',
+    weex: 'readonly',
+  },
+  rules: {
+    'no-console':
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'pre-production' ||
+      process.env.NODE_ENV === 'staging'
+        ? 'warn'
+        : 'off',
+    'no-debugger':
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'pre-production' ||
+      process.env.NODE_ENV === 'staging'
+        ? 'warn'
+        : 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'state', // for vuex state
+          'acc', // for reduce accumulators
+          'e', // for e.returnvalue
+        ],
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars':
+      process.env.NODE_ENV === 'production' ||
+      process.env.NODE_ENV === 'pre-production' ||
+      process.env.NODE_ENV === 'staging'
+        ? 'warn'
+        : 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ['shims-tsx.d.ts'],
+      rules: {
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.mjs', '.ts', '.tsx'],
+    'import/resolver': {
+      // https://github.com/benmosher/eslint-plugin-import/issues/1396
+      [require.resolve('eslint-import-resolver-node')]: {},
+      [require.resolve('eslint-import-resolver-webpack')]: {
+        config: require.resolve('@vue/cli-service/webpack.config.js'),
+      },
+    },
+  },
+};
